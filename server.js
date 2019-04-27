310a19dbf7c16493aad7b5d0ce194bcd8431e031
Jitlan <jliebling1994@gmail.com>
2019-04-27 12:03:34 -0400

    First big commit for personal site.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express'); 
+const app = express();
+ 
+app.use(express.static('project'));
+
+//make way for some custom css, js and images
+app.use('/css', express.static(__dirname + '/project/css'));
+app.use('/ts', express.static(__dirname + '/project/ts'));
+app.use('/images', express.static(__dirname + '/project/images'));
+
+var server = app.listen(8081, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
