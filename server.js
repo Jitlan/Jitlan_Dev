d56cbd77b4a6c43c3feb2fa06a0b3227729d9c38
Jitlan <jliebling1994@gmail.com>
2019-04-28 15:05:11 -0400

    responsiveness changes and some QoL stuff

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express'); 
 const app = express();
  
-app.use(express.static('project'));
+app.use(express.static('dist'));
 
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/project/css'));
